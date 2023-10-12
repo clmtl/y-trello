e3aaaa06536d27599f67b7255bcfd73b99a0857e
GuyMickael <mickael.guy.pro@gmail.com>
2023-10-12 16:03:59 +0200

    commiting changes

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -8,29 +8,40 @@ import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 import { Priority } from "@prisma/client";
 
 interface Props {
+  id: string;
   title: string;
   description: string;
   priority: string;
   state: string;
 }
 
-const TaskCard: React.FC<Props> = ({ title, description, priority, state }) => {
+const TaskCard: React.FC<Props> = ({
+  id,
+  title,
+  description,
+  priority,
+  state,
+}) => {
   const [popupOpen, setPopupOpen] = useState(false);
   const changeState = () => {
     console.log("changeState");
   };
+  const [tempTitle, setTempTitle] = useState(title);
+  const [tempDescription, setTempDescription] = useState(description);
+  const [tempPriority, setTempPriority] = useState(priority);
+  const [tempState, setTempState] = useState(state);
 
   const returnPiorityIcon = () => {
     if (priority === Priority.HIGHEST)
-      return <MdKeyboardDoubleArrowUp className="text-red-800" />;
+      return <MdKeyboardDoubleArrowUp className="text-xl text-red-800" />;
     if (priority === Priority.HIGH)
-      return <MdKeyboardArrowUp className="text-orange-500" />;
+      return <MdKeyboardArrowUp className="text-xl text-orange-500" />;
     if (priority === Priority.MEDIUM)
-      return <AiOutlineMinus className="text-white" />;
+      return <AiOutlineMinus className="text-xl text-white" />;
     if (priority === Priority.LOW)
-      return <MdKeyboardArrowDown className="text-blue-600" />;
+      return <MdKeyboardArrowDown className="text-xl text-blue-600" />;
     if (priority === Priority.LOWEST)
-      return <MdKeyboardDoubleArrowDown className="text-green-800" />;
+      return <MdKeyboardDoubleArrowDown className="text-xl text-green-800" />;
   };
 
   return (
@@ -46,15 +57,35 @@ const TaskCard: React.FC<Props> = ({ title, description, priority, state }) => {
         </div>
       </div>
       {popupOpen ? (
-        <div className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-black bg-opacity-50 p-14 text-gray-300">
           <div className="h-1/2 w-1/2 rounded-lg bg-gray-700">
             <div className="flex items-center justify-between">
               <p className="text-white">{title}</p>
               <button onClick={() => setPopupOpen(false)}>X</button>
             </div>
-            <p>{description}</p>
-            <p>{priority}</p>
-            <p>{state}</p>
+            <div className="m-2 flex flex-col border-b-2 border-t-2 pb-2 text-xl">
+              {" "}
+              <p className="flex">
+                <BsTextLeft className="text-2xl" /> Description
+              </p>
+              <textarea value={description} className="bg-slate-500 text-sm" />
+            </div>
+            <p className="m-2 text-xl">Priority </p>
+            <div className="m-2 flex border-b-2">
+              <p className="m-2 flex pl-4">
+                {priority} {returnPiorityIcon()}
+              </p>
+              <p className="m-2 flex pl-4">Update Priority : </p>
+              <select name="Update Priority" id="cars" className="bg-gray-600">
+                <option value="HIGHEST">Highest</option>
+                <option value="HIGH">High</option>
+                <option value="MEDIUM">Medium</option>
+                <option value="LOW">Low</option>
+                <option value="LOWEST">Lowest</option>
+              </select>
+            </div>
+
+            <p>State : {state}</p>
             <button onClick={changeState}>Change state</button>
           </div>
         </div>
