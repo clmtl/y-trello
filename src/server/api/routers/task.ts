218d54ec6070c1c3fd868b61a68e87ccf84b7b15
clmtl <bk201li@protonmail.com>
2023-10-13 10:53:34 +0200

    wip

--- src/server/api/routers/task.ts
+++ src/server/api/routers/task.ts
@@ -1,59 +1,69 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
-import { State, Priority } from "@prisma/client"
+import { State, Priority } from "@prisma/client";
 
 export const taskRouter = createTRPCRouter({
   getTaskById: protectedProcedure
-  .input(z.object({ id: z.string() }))
-  .query(async ({ ctx, input,  }) => {
-    const task = await ctx.db.task.findUnique({
-      where: { id: Number(input.id) },
-    });
+    .meta({ openapi: { method: "GET", path: "/task" } })
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const task = await ctx.db.task.findUnique({
+        where: { id: Number(input.id) },
+      });
 
-    return task;
-  }),
+      return task;
+    }),
 
-  getAll: protectedProcedure.query(async ({ ctx }) => {
-    const task = await ctx.db.task.findMany({
-      take: 100,
-      orderBy: [{ createdAt: "desc" }],
-    });
+  getAll: protectedProcedure
+    .meta({ openapi: { method: "GET", path: "/task" } })
+    .query(async ({ ctx }) => {
+      const task = await ctx.db.task.findMany({
+        take: 100,
+        orderBy: [{ createdAt: "desc" }],
+      });
 
-    return task;
-  }),
+      return task;
+    }),
 
   create: protectedProcedure
-  .input(z.object({ title: z.string() }))
-  .mutation(async ({ ctx, input }) => {
-    const task = await ctx.db.task.create({
-      data:{
-        title : input.title
-      },
-    });
+    .meta({ openapi: { method: "POST", path: "/task" } })
+    .input(z.object({ title: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.db.task.create({
+        data: {
+          title: input.title,
+        },
+      });
 
-    return task
-  }),
+      return task;
+    }),
 
   update: protectedProcedure
-  .input(z.object({ id: z.string(), title: z.string(), description: z.string(), state: z.nativeEnum(State), priority: z.nativeEnum(Priority) }))
-  .mutation(async ({ ctx, input }) => {
-    const task = await ctx.db.task.update({
-      where: {
-        id: Number(input.id),
-      },
-      data: {
-        title: input.title,
-        description: input.description,
-        state: input.state,
-        priority: input.priority
-      }
-    });
+    .meta({ openapi: { method: "PATCH", path: "/task" } })
+    .input(
+      z.object({
+        id: z.string(),
+        title: z.string(),
+        description: z.string(),
+        state: z.nativeEnum(State),
+        priority: z.nativeEnum(Priority),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const task = await ctx.db.task.update({
+        where: {
+          id: Number(input.id),
+        },
+        data: {
+          title: input.title,
+          description: input.description,
+          state: input.state,
+          priority: input.priority,
+        },
+      });
 
-    return task
-  }),
+      return task;
+    }),
 });
